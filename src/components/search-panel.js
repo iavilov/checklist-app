8be7f8f6020e2804b2d6cb3c3e41ca1b20df5750
Avilov Ivan <avilov.inbox@gmail.com>
2019-07-25 20:33:45 +0300

    start

---
+++ src/components/search-panel.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const SearchPanel = () => {
+
+    const searchText = 'Type here to search';
+    const searchStyle = {
+        fontSize: '25px'
+    };
+
+    return <input
+        style={searchStyle}
+        placeholder={searchText}
+        className="foo" />
+}
+
+export default SearchPanel;
